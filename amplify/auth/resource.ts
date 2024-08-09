e944c1d1174ba97e6bc7bedbe6156f8b59e9c472
Daniel Son <dson.email@gmail.com>
2024-08-09 16:30:34 +1000

    git commit -am "added authenticator, changed auth email" git push

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth } from "@aws-amplify/backend";
 
 /**
  * Define and configure your auth resource
@@ -6,6 +6,10 @@ import { defineAuth } from '@aws-amplify/backend';
  */
 export const auth = defineAuth({
   loginWith: {
-    email: true,
+    email: {
+      verificationEmailSubject: "Verifyyy todo!",
+      verificationEmailBody: (createCode: (text?: string) => string) =>
+        `Todo heyyy, ${createCode()} is your verification code!`,
+    },
   },
 });
